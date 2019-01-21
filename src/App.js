5ff4161a670c5582d257eba88b5cda3b7fe4cd99
Emika Ijuin <emikaijuin@gmail.com>
2019-01-21 17:32:32 +0800

    first commit - initial working tic tac toe game

--- src/App.js
+++ src/App.js
@@ -3,23 +3,127 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentPlayer: "X",
+      X_positions: [],
+      O_positions: [],
+      win: false,
+      draw: false,
+      canReset: false,
+      message: "X's turn."
+    }
+  }
+
+  play = e => {
+    if (e.target.innerHTML) { return "Invalid move." }
+    this.markSquare(e.target)
+    this.updatePositions(parseInt(e.target.id))
+    this.setState({
+      win: this.checkWin(),
+      draw: this.checkDraw()
+    }, () => this.next())
+  }
+
+  next = () => {
+    let message
+    if (this.state.win) {
+      message = `Congratulations, player ${this.state.currentPlayer} won!`
+      this.setState({canReset: true})
+    } else if (this.state.draw) {
+      message = 'Game is a draw.'
+      this.setState({canReset: true})
+    } else {
+      this.setState({currentPlayer: this.toggleCurrentPlayer() })
+      message = `${this.state.currentPlayer == "X" ? "O" : "X"}'s turn.`
+    }
+    this.setState({message: message})
+  }
+
+  setMessage = message => document.getElementById('message').innerHTML = message
+
+  markSquare = elem => elem.innerHTML = this.state.currentPlayer
+
+  updatePositions = (index) => {
+    let currentPositions = this.currentPositions()
+    let updatedPositionState = {}
+    currentPositions.push(index)
+    updatedPositionState[`${this.state.currentPlayer}_positions`] = currentPositions
+    
+    return updatedPositionState
+  }
+
+  toggleCurrentPlayer = () => this.state.currentPlayer === "X" ? "O" : "X"
+
+  checkWin = () => {
+    let win = false
+    this.winningPositions().forEach(winPos => {
+      if ( 
+        this.currentPositions().includes(winPos[0]) && 
+        this.currentPositions().includes(winPos[1]) && 
+        this.currentPositions().includes(winPos[2])
+      ) {
+        win = true
+      }
+    })
+    return win
+  }
+
+  checkDraw = () => {
+    if ([...this.state.X_positions, ...this.state.O_positions].length === 9 && !this.state.win) {
+      return true
+    } 
+    return false
+  }
+
+  currentPositions = () => this.state[`${this.state.currentPlayer}_positions`]
+
+  winningPositions() {
+    return [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+  }
+
+  resetGame = () => {
+    Array.from(document.querySelector('.board').children).forEach(elem => {
+      elem.innerHTML = ""
+    })
+    this.setState({
+      currentPlayer: "X",
+      X_positions: [],
+      O_positions: [],
+      win: false,
+      draw: false,
+      message: `${this.state.currentPlayer}'s turn.`,
+      canReset: false
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="message"> { this.state.message }</div>
+        <button disabled={ !this.state.canReset } onClick={this.resetGame}>Reset Game</button>
+        <div className="board" onClick={this.play}>
+          <div id="0" className="square"></div>
+          <div id="1" className="square"></div>
+          <div id="2" className="square"></div>
+          <div id="3" className="square"></div>
+          <div id="4" className="square"></div>
+          <div id="5" className="square"></div>
+          <div id="6" className="square"></div>
+          <div id="7" className="square"></div>
+          <div id="8" className="square"></div>
+        </div>
       </div>
     );
   }
